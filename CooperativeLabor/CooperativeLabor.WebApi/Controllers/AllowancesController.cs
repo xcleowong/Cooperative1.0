60266f9e5196f3f28ef11d3373fdb0c172e85aae
xcleowong <xcleowong@users.noreply.github.com>
2019-01-09 08:56:45 +0800

    xcleowong

--- CooperativeLabor/CooperativeLabor.WebApi/Controllers/AllowancesController.cs
+++ CooperativeLabor/CooperativeLabor.WebApi/Controllers/AllowancesController.cs
@@ -61,17 +61,17 @@ namespace CooperativeLabor.WebApi.Controllers
         /// <returns></returns>
         [Route("GetAllowances")]
         [HttpGet]
-        public PageNumber GetAllowances(int pageIndex)
+        public PageNumber GetAllowances(int? pageIndex)
         {
-            if(pageIndex==null)
+            if (pageIndex == null)
             {
                 pageIndex = 1;
             }
             List<Allowances> listGA = allowancesServices.GetAllowances().ToList();
             PageNumber pageNumber = new PageNumber();
-            pageNumber.CurrentPage = pageIndex;
+            pageNumber.CurrentPage = Convert.ToInt32(pageIndex);
             pageNumber.TotlePage = (listGA.Count / PAGESIZE) + (listGA.Count % PAGESIZE == 0 ? 0 : 1);
-            pageNumber.Data = listGA.Skip((pageIndex-1)*PAGESIZE).Take(PAGESIZE);
+            pageNumber.Data = listGA.Skip(( Convert.ToInt32(pageIndex) - 1) * PAGESIZE).Take(PAGESIZE);
             return pageNumber;
         }
 
