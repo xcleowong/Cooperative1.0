addfd71d01b858178a3a6406a28d765d6061dae3
LY123 <1303032089@qq.com>
2019-01-15 09:46:20 +0800

    liuyi

---
+++ CooperativeLabor/CooperativeLabor.Mvc/Controllers/AttendanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CooperativeLabor.Mvc.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        // GET: Attendance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
