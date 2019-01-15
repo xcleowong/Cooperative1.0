c561cc75e01a676e3a54ab2e8b4eb0ae501d74a3
changxy0116 <45485958+changxy0116@users.noreply.github.com>
2019-01-15 09:48:27 +0800

    cxy

--- CooperativeLabor/CooperativeLabor.WebApi/Controllers/EntryDimissionController.cs
+++ CooperativeLabor/CooperativeLabor.WebApi/Controllers/EntryDimissionController.cs
@@ -13,7 +13,7 @@ namespace CooperativeLabor.WebApi.Controllers
     [RoutePrefix("EntryDimission")]
     public class EntryDimissionController : ApiController
     {
-        private const int PAGESIZE = 8;
+        private const int PAGESIZE = 6;
         [Dependency]
         public IEntryDimissionRecordServices entryDimission { get; set; }
 
@@ -96,10 +96,22 @@ namespace CooperativeLabor.WebApi.Controllers
         /// <returns></returns>
         [Route("GetPersonals")]
         [HttpGet]
-        public List<PersonalInformation> GetPersonals()
+        public PageNumber GetPersonals(int? pageIndex,string name="")
         {
-            var result = entryDimission.GetPersonals();
-            return result;
+            if (pageIndex == null)
+            {
+                pageIndex = 1;
+            }
+            List<PersonalInformation> listGA = entryDimission.GetPersonals().ToList();
+            PageNumber pageNumber = new PageNumber();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                listGA = listGA.Where(s => s.Name.Contains(name)).ToList();
+            }
+            pageNumber.CurrentPage = Convert.ToInt32(pageIndex);
+            pageNumber.TotlePage = (listGA.Count / PAGESIZE) + (listGA.Count % PAGESIZE == 0 ? 0 : 1);
+            pageNumber.Data = listGA.Skip((Convert.ToInt32(pageIndex) - 1) * PAGESIZE).Take(PAGESIZE);
+            return pageNumber;
         }
     }
 }
