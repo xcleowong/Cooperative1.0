885a0f0c596db96cfce257c6c4e99ef38a751581
FengYiPeng <fengajin@126.com>
2019-01-19 19:26:53 +0800

    01/19

--- CooperativeLabor/CooperativeLabor.Mvc/Controllers/Approval/ApprovalActivityController.cs
+++ CooperativeLabor/CooperativeLabor.Mvc/Controllers/Approval/ApprovalActivityController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace CooperativeLabor.Mvc.Controllers.shenpi
+namespace CooperativeLabor.Mvc.Controllers.Approval
 {
     public class ApprovalActivityController : Controller
     {
