21de6f590c69f4b0996cc0db576d88494d422db1
FengYiPeng <fengajin@126.com>
2019-01-19 20:27:57 +0800

    01/19

--- CooperativeLabor/CooperativeLabor.WebApi/Controllers/Approval/ApprovalActivityController.cs
+++ CooperativeLabor/CooperativeLabor.WebApi/Controllers/Approval/ApprovalActivityController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace CooperativeLabor.WebApi.Controllers.shenpi
+namespace CooperativeLabor.WebApi.Controllers.Approval
 {
     using CooperativeLabor.Model;
     using CooperativeLabor.IServices;
